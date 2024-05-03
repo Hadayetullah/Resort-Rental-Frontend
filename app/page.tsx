6429b7bb538aac94bbca4435c355339a976c47d6
Hadayetullah <hadayetullah30@gmail.com>
2024-05-03 23:37:02 +0600

    Started working on Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="">
       This is a start
-      <h2 className="text-airbnb">This is second text</h2>
+      <h2 className="text-primary">This is second text</h2>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        'airbnb': '#ff385c',
-        'airbnb-dark': '#d50027'
+        'primary': '#ff385c',
+        'secondary': '#d50027'
       }
     },
   },
