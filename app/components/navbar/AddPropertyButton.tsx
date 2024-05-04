e57417a6aa7ab0db421e73886949acf00413abe9
Hadayetullah <hadayetullah30@gmail.com>
2024-05-04 18:51:50 +0600

    Designed on categories

--- app/components/navbar/AddPropertyButton.tsx
+++ app/components/navbar/AddPropertyButton.tsx
@@ -1,7 +1,7 @@
 const AddPropertyButton = () => {
   return (
     <div className="p-2 cursor-pointer text-semibold text-sm rounded-full hover:bg-gray-200">
-      It is your home
+      It's your home
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
+import Categories from "./components/Categories";
 
 export default function Home() {
   return (
-    <main className="">
-      This is a start
-      <h2 className="text-primary">This is second text</h2>
+    <main className="max-w-[1500] mx-auto px-6">
+      <Categories />
     </main>
   );
 }
